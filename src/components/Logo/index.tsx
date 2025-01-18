cde04926332b465686bb60541b36a938a0599e17
Grigoriy Reshetniak <grigoriy.reshetniak@gmail.com>
2025-01-18 14:48:22 +0200

    fix reset quiz a little

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,9 +1,8 @@
 import logo from '../../../assets/images/go-logo-white.svg';
 
 export const Logo = ({ handleReset }: { handleReset?: () => void }) =>
-  <div className="logo">
-    <img src={logo} alt={"Golang logo"} onClick={handleReset ?? (() => {})}/>
-    <span onClick={handleReset ?? (() => {})}>QUIZ</span>
+  <div className="logo" onClick={handleReset ?? (() => {})}>
+    <img src={logo} alt={"Golang logo"}/>
+    <span>QUIZ</span>
   </div>
 ;
-
