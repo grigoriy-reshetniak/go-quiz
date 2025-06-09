56cec34d132ef078eb99a977b0fb370983fac6ec
Grigoriy Reshetniak <grigoriy.reshetniak@gmail.com>
2025-06-09 15:36:55 +0300

    rework footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,23 +1,12 @@
 export const Footer = () => {
   return (
     <footer>
-      <div className="subsection">
-        <a href="https://github.com/grigoriy-reshetniak" target="_blank" rel="noopener noreferrer">
-          by Grigoriy Reshetniak
-        </a>
-        <a
-          href="https://www.buymeacoffee.com/"
-          target="_blank"
-          rel="noopener"
-          title="Buy a coffee for Grigoriy Reshetniak"
-        >
-          <img
-            src="https://cdn.buymeacoffee.com/buttons/v2/default-blue.png"
-            alt="Buy Me A Coffee"
-            className="by-me-a-coffee"
-          />
-        </a>
-      </div>
+      <a href="https://github.com/grigoriy-reshetniak" target="_blank" rel="noopener noreferrer">
+        by Grigoriy Reshetniak
+      </a>
+      <a href="https://github.com/KiteShi" target="_blank" rel="noopener noreferrer">
+        Questions by Kateryna Azarenko
+      </a>
       <a
         href="https://github.com/grigoriy-reshetniak/go-quiz"
         target="_blank"
@@ -25,26 +14,23 @@ export const Footer = () => {
         className="github"
       >
         <img src="../../../assets/images/github-logo-light.svg" alt="Github logo"/>
-        <span>github</span>
+        <span>source code</span>
       </a>
       <span>2025</span>
-      <div className="subsection">
-        <a
-          href="https://www.buymeacoffee.com/"
-          target="_blank"
-          rel="noopener"
-          title="Buy a coffee for Kateryna Azarenko"
-        >
-          <img
-            src="https://cdn.buymeacoffee.com/buttons/v2/default-blue.png"
-            alt="Buy Me A Coffee"
-            className="by-me-a-coffee"
-          />
-        </a>
-        <a href="https://github.com/KiteShi" target="_blank" rel="noopener noreferrer">
-          Questions by Kateryna Azarenko
-        </a>
-      </div>
+      <a
+        href="https://buymeacoffee.com/grigoriy.reshetniak"
+        target="_blank"
+        rel="noopener"
+        title="Suport us"
+        aria-label="Buy us a cofee"
+        className="buy-me-a-coffee"
+      >
+        <img
+          src="../../../assets/images/buy-me-a-coffee-logo.svg"
+          alt="Buy Me A Coffee icon"
+        />
+        <span>Buy us a coffee</span>
+      </a>
     </footer>
   );
 };
